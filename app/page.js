4f551f21df3b39d38193c2081314fdd6897a3a9b
Pranav Sharma <dev-021@icingtree.com>
2024-12-09 18:00:41 +0530

    intro animation

--- app/page.js
+++ app/page.js
@@ -6,101 +6,31 @@ import { ScrollTrigger } from "gsap/ScrollTrigger";
 import { useGSAP } from "@gsap/react";
 
 export default function Home() {  
-  const [sparkles,setsparkles]=useState([
-    {
-      x:0,
-      y:0
-    },
-    {
-      x:0,
-      y:10
-    },
-    {
-      x:0,
-      y:20
-    },
-    {
-      x:0,
-      y:30
-    },
-    {
-      x:0,
-      y:40
-    },
-    {
-      x:0,
-      y:50
-    },
-    {
-      x:0,
-      y:60
-    },
-    {
-      x:0,
-      y:70
-    },
-    {
-      x:0,
-      y:80
-    },
-    {
-      x:0,
-      y:90
-    },
-    {
-      x:0,
-      y:100
+  const [sparkles,setsparkles]=useState([])
+  const [sparkleColors,setSparkleColors] = useState(["#08090b","#0c6da1","#0caad8","#0b5771","#09375a","#061b33","#09303c","#657171","#373a3c","#748c8c"])
+  const SparklesGeneration = (number)=>{
+    let arr = []
+
+    for(let i=0;i<number;i++){
+      const x=  Math.round(Math.random()*100)
+      const y=  Math.round(Math.random()*100)
+      const size=Math.round(Math.random()*10)
+      const delay = Math.random()*1
+      const duration = Math.random()*2
+      arr.push({x:x,y:y,size:size,delay:delay,duration:duration})
     }
-  ])
+    return arr
+  }
+  useEffect(()=>{
+    setsparkles(SparklesGeneration(200))
+  },[])
   useGSAP(()=>{
-    // .to('#bee-animate2', {
-    //   paddingLeft:'25%',
-    // },0).to('#bee-animate2',{
-    //   opacity: 1,
-    // },0.5).to('#bee-animate3',{
-    //   left:'25%',
-    //   paddingLeft:'25%',
-    // },1).to('#bee-animate3',{
-    //   opacity: 1,
-    // },1.5)
-    if (typeof window !== "undefined") {
-      gsap.registerPlugin(ScrollTrigger);
-      const bee = gsap.timeline();
-      bee.to('#blast',{
-        width: "100vh",
-        height: "100vh",
-      }).to('#cover',{
-        opacity:1
-      })
-      ScrollTrigger.create({
-          animation: bee,
-          trigger: "#bee-animate",
-          start: "top top",
-          end: "+=1000vh center",
-          scrub: 2,
-          pin:'#bee-animate',
-          markers:false,
-          pinSpacer:false
-      })
-    }
+
   })
-
-
+  console.log();
+  
   return (
     <div id="bee-animate" className={styles.bee_animate}>
-    {/* 
-    <div id="bee-animate1" className={`${styles['bee-animate1']} section`}>
-    <img className={styles['bee-animate1-image']}  src="/creatives/bumblebee.png"/>
-    </div>
-    <div id="bee-animate2" className={`${styles['bee-animate2']} section`}>
-    <img className={styles['bee-animate2-image']} src="/creatives/bumblebee_active.png"/>
-    </div>
-    <div id="bee-animate3" className={`${styles['bee-animate3']} section`}>
-    <div className="relative">
-    <img className={styles['bee-animate3-image']}  src="/creatives/bumblebee_active_2.png"/>
-    </div>
-    </div> 
-    */}
     <div id="blast" className={styles.blast}></div>
     {
       sparkles.map((data,i)=>(
@@ -108,13 +38,23 @@ export default function Home() {
         style={{
           "--sparkles-top": `${data.x}%`,
           "--sparkles-left": `${data.y}%`,
+          "--sparkles-size": `${data.size}vh`,
+          "--sparkles-delay": `${data.delay}s`,
+          "--sparkles-duration": `${data.duration}s`,
+          backgroundColor:`${sparkleColors[Math.round(Math.random(sparkleColors)*sparkleColors.length)]}`,
+          boxShadow:`0px 0px 1px 4px ${sparkleColors[Math.round(Math.random(sparkleColors)*sparkleColors.length)]}`
         }} className={styles.sparkles}></div>
       ))
     }
-    {/* <div id="cover" className={styles.cover}>
+    <div id="cover" className={styles.cover}>
+      <div className={`${styles.content} grid place-items-center relative h-full w-full`}>
+        <div>
       <div className={styles.caption}>This is your's truly</div>
       <div className={styles.name}>Pranav Sharma</div>
-    </div> */}
+      </div>
+      </div>
+     
+    </div>
     </div>
   )
 }
