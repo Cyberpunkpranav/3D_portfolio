b6dad58d94f384dd2070bc257834bf73974368b3
Pranav Sharma <dev-021@icingtree.com>
2024-12-11 18:26:04 +0530

    123

--- app/page.js
+++ app/page.js
@@ -4,6 +4,7 @@ import styles from "./page.module.css";
 import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 import { useGSAP } from "@gsap/react";
+import Image from "next/image";
 
 export default function Home() {  
   const [sparkles,setsparkles]=useState([])
@@ -43,25 +44,54 @@ export default function Home() {
     //   width: '0%',
     // },1)
     t1.to('#content',{
-      z:800,
-    }).to('#content',{
-      color:'white'
-    }).to('#circle1',{
-      height:'20vh',
-      width:'20vh'
-    }).to('#circle2',{
-      height:'15vh',
-      width:'15vh'
-    }).to('#content',{
-      z:0,
+      rotateX:60,
+      rotateY:60,
+      z:500
+    },0)
+    .to('#content',{
+      rotateX:75,
+      rotateY:75,
+      z:600
+    },0)
+    .to('#content',{
+      rotateX:90,
+      rotateY:90,
+      z:700
+    },0)
+    .to('#rectangle1',{
+      height:'5px',
     })
+    .to('#content',{
+      opacity:0
+    })
+    .to('#content',{
+      rotateX:0,
+      rotateY:0,
+      z:0
+    })
+    .to('#rectangle1',{
+      height:'100vh',
+    })
+    .to('#content',{
+      z:0,
+    }).to('#front',{
+     height:0
+    }).to('#back',{
+      height:0
+     })
+     .to('#rectangle1',{
+      z:1000
+     })
+     .to('#experience',{
+      height:'100vh'
+     })
     ScrollTrigger.create({
       animation:t1,
       start:'1vh center',
       trigger: "#content",
-      end:'+=1000vh top',
+      end:'+=4000vh top',
       pin:'#portfolio',
-      scrub:3,
+      scrub:1,
       markers:false
     })
   },[])
@@ -87,13 +117,94 @@ export default function Home() {
       ))
     }
     <div id="cover" className={styles.cover}>
-      <div  className={`${styles.content} grid place-items-center relative h-full w-full`}>
-      <div id="content" className="relative p-5">
-      <div className={`${styles.caption}`}>This is your's truly</div>
-      <div className={`${styles.name}`}>Pranav Sharma</div>
-      <div id="circle1" className={styles.circle1}></div>
-      <div id="circle2" className={styles.circle2}></div>
+      <div  className={`${styles.content} grid place-items-center h-full w-full`}>
+      <div id="content" className="p-5">
+      <div id="caption" className={`${styles.caption}`}>This is your's truly</div>
+      <div id="name" className={`${styles.name}`}>Pranav Sharma</div>
       </div>
+      <div id="rectangle1" className={styles.rectangle1}>
+        <div id="stack" className="h-full w-full grid place-items-center overflow-hidden">
+        <div className={styles.stack}>
+          <div className={styles.techstacks}>
+          <h1 className={styles.front} id="front">FrontEnd</h1>
+          <h1 className={styles.back} id="back"> BackEnd</h1>
+          <h1 className={styles.full} id="full"> FullStack</h1>
+          </div>
+          <div>
+            <h1>Engineer</h1>
+          </div>
+        </div>
+        </div>
+        <div className={` ${styles.tech_container}`}>   
+      <div className={`flex mt-[30px] relative overflow-hidden gap-x-5 py-5 ${styles.techs}`} style={{ '--time': '60s' }}>
+      <article className='flex items-center'>
+      <div><Image alt='html' width={100} height={100} src='/logo/html.png'/></div>
+      <div><Image alt='css' width={100} height={100} src='/logo/css.png'/></div>
+      <div><Image alt='css' width={100} height={100} src='/logo/js.png'/></div>
+      <div><Image alt='css' width={100} height={100} src='/logo/jQuery.png'/></div>
+      <div><Image alt='css' width={100} height={100} src='/logo/bootstrap.png'/></div>
+      <div><Image alt='css' width={100} height={100} src='/logo/tailwind.png'/></div>
+      <div><Image alt='css' width={100} height={100} src='/logo/reactjs.png'/></div>
+      <div><Image alt='css' width={100} height={100} src='/logo/nodejs.png'/></div>
+      <div><Image alt='css' width={100} height={100} src='/logo/nextjs.png'/></div>
+      <div><Image alt='css' width={100} height={100} src='/logo/expressjs.png'/></div>
+      <div><Image alt='css' width={100} height={100} src='/logo/golang.png'/></div>
+      <div><Image alt='css' width={100} height={100} src='/logo/mysql.png'/></div>
+      <div><Image alt='css' width={100} height={100} src='/logo/aws.png'/></div>
+      </article>
+      <article className='flex items-center'>
+      <div><Image alt='html' width={100} height={100} src='/logo/html.png'/></div>
+      <div><Image alt='css' width={100} height={100} src='/logo/css.png'/></div>
+      <div><Image alt='css' width={100} height={100} src='/logo/js.png'/></div>
+      <div><Image alt='css' width={100} height={100} src='/logo/bootstrap.png'/></div>
+      <div><Image alt='css' width={100} height={100} src='/logo/tailwind.png'/></div>
+      <div><Image alt='css' width={100} height={100} src='/logo/reactjs.png'/></div>
+      <div><Image alt='css' width={100} height={100} src='/logo/nodejs.png'/></div>
+      <div><Image alt='css' width={100} height={100} src='/logo/nextjs.png'/></div>
+      <div><Image alt='css' width={100} height={100} src='/logo/expressjs.png'/></div>
+      <div><Image alt='css' width={100} height={100} src='/logo/golang.png'/></div>
+      <div><Image alt='css' width={100} height={100} src='/logo/mysql.png'/></div>
+      <div><Image alt='css' width={100} height={100} src='/logo/aws.png'/></div>
+      </article>
+      </div>
+      <div className={`flex relative gap-x-5 overflow-hidden py-5 ${styles.techs}`} style={{ '--time': '60s' }}>
+      <article className='flex items-center'>
+      <div><Image alt='css' width={100} height={100} src='/logo/aws.png'/></div>
+      <div><Image alt='css' width={100} height={100} src='/logo/mysql.png'/></div>
+      <div><Image alt='css' width={100} height={100} src='/logo/golang.png'/></div>
+      <div><Image alt='css' width={100} height={100} src='/logo/expressjs.png'/></div>
+      <div><Image alt='css' width={100} height={100} src='/logo/nextjs.png'/></div>
+      <div><Image alt='css' width={100} height={100} src='/logo/nodejs.png'/></div>
+      <div><Image alt='css' width={100} height={100} src='/logo/reactjs.png'/></div>
+      <div><Image alt='css' width={100} height={100} src='/logo/tailwind.png'/></div>
+      <div><Image alt='css' width={100} height={100} src='/logo/bootstrap.png'/></div>
+      <div><Image alt='css' width={100} height={100} src='/logo/jQuery.png'/></div>
+      <div><Image alt='css' width={100} height={100} src='/logo/js.png'/></div>
+      <div><Image alt='css' width={100} height={100} src='/logo/css.png'/></div>
+      <div><Image alt='html' width={100} height={100} src='/logo/html.png'/></div>
+      </article>
+      <article className='flex items-center'>
+      <div><Image alt='css' width={100} height={100} src='/logo/aws.png'/></div>
+      <div><Image alt='css' width={100} height={100} src='/logo/mysql.png'/></div>
+      <div><Image alt='css' width={100} height={100} src='/logo/golang.png'/></div>
+      <div><Image alt='css' width={100} height={100} src='/logo/expressjs.png'/></div>
+      <div><Image alt='css' width={100} height={100} src='/logo/nextjs.png'/></div>
+      <div><Image alt='css' width={100} height={100} src='/logo/nodejs.png'/></div>
+      <div><Image alt='css' width={100} height={100} src='/logo/reactjs.png'/></div>
+      <div><Image alt='css' width={100} height={100} src='/logo/tailwind.png'/></div>
+      <div><Image alt='css' width={100} height={100} src='/logo/bootstrap.png'/></div>
+      <div><Image alt='css' width={100} height={100} src='/logo/jQuery.png'/></div>
+      <div><Image alt='css' width={100} height={100} src='/logo/js.png'/></div>
+      <div><Image alt='css' width={100} height={100} src='/logo/css.png'/></div>
+      <div><Image alt='html' width={100} height={100} src='/logo/html.png'/></div>
+      </article>
+      </div>
+      </div>
+      </div>
+      <div id="experience" className={styles.experience}>
+        <h1>Experience</h1>
+      </div>
+      {/* <div id="circle2" className={styles.circle2}></div> */}
       </div>
     </div>
     </div>
