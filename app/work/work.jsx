9beacf9209516215cd43a8f18ad8df421214b4de
Pranav Sharma <pranavsharma733902@gmail.com>
2025-01-28 18:33:51 +0530

    work content

--- app/work/work.jsx
+++ app/work/work.jsx
@@ -11,8 +11,8 @@ const Work = () => {
         <div className="relative h-full w-full">
         </div>
       </div>
-      <h2 className='w-full'>SpacePe LLP</h2>
-      <h3>techrajendra.com</h3>
+      <h2 className='w-full flex items-end justify-between'>SpacePe LLP<p className='inline-block text-brown'>may 2024 - current</p></h2>
+      <h3><span className='mr-5'>1.</span>techrajendra.com</h3>
       <main>
         <aside>
           <div className='relative'>
@@ -25,23 +25,42 @@ const Work = () => {
         <article>
         <h5>About techrajendra.com</h5>
       <p>techrajendra.com is a site where clients can see and have faith in the company&#39;s team that we can deliver the projects. The products we are working and delivered, are smooth and good to use. Using animations, I made the website more catchy, and the product pages show the exact information and capability of our products.</p>
-      <h5>Tech Stack</h5>
-      <p className='flex flex-wrap justify-between gap-y-10'><span>Next JS</span><span>Scss</span><span>Tailwind CSS</span><span>Wordpress API</span><span>GSAP</span></p>
+      <div className="flex flex-wrap gap-10 items-start mt-10">
+      <div>
+      <h6 className='text-red'>Main Stack</h6>
+      <p>Next JS</p>
+      <p>Scss</p>
+      <p>Tailwind CSS</p>
+      <p>GSAP</p>
+      </div>
+      <div>
+      <h6 className='text-red'>Contribution</h6>
+      <p>Solo project</p>
+      <p>SEO integration</p>
+      <p>WordPress API integration</p>
+      </div>
+      <div>
+      <h6 className='text-red'>Date</h6>
+      <p>2024-2025</p>
+      </div>
+      </div>
+    
+
       </article>
       </main>
       <main>
       <article>
-      <h5>About CRM Dashboard</h5>
-            <p> In Customer Relationship Management , executive needs to create a lead in order to solve customer problems.</p>
-            <p> Leads will get assigned to that corresponding task to execute it.</p>
-            <p>If any customer still gets the issue , the executive could also create a complaint against a task or lead in it.</p>
-            <p>Customers account lists with custom search facility</p>
-            <p>Account statements of each customer to get the information of ones statements</p>
-            <p>Customer explained detail to help executive to know customer better</p>
-            <p>Facility to download excel sheet of leads, tasks and complaints to help executive to complete their jobs.</p>
-            <p>I contribute in Rule Based Access - RBA feature along with the project manager to make it happen in front end as well as the backend structure </p>
-            <p>It filters up the customers on the based conditions for choosen offer</p>
-            <p>Executive can download the filtered customers to call and explain the offer to them</p>
+      <h5>About techrajendra CRM </h5>
+            <p> In Customer Relationship Management , executive needs to create a lead in order to solve customer problems.
+             Leads will get assigned to that corresponding task to execute it.
+            If any customer still gets the issue , the executive could also create a complaint against a task or lead in it.
+            Customers account lists with custom search facility.
+            Account statements of each customer to get the information of ones statements
+            Customer explained detail to help executive to know customer better
+            Facility to download excel sheet of leads, tasks and complaints to help executive to complete their jobs.
+            I contribute in Rule Based Access - RBA feature along with the project manager to make it happen in front end as well as the backend structure
+            It filters up the customers on the based conditions for choosen offer
+            Executive can download the filtered customers to call and explain the offer to them.</p>
       </article>
       <aside>
         <div className="relative">
